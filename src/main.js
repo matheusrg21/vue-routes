7b99ec6f04f91c377ba1929bf4560f7b7b4dbf17
Matheus Rodrigues <matheusrg21@hotmail.com>
2021-07-07 20:37:51 -0300

    Inicio da Seção Vue Router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import Home from './views/Home.vue'
+import Contatos from './views/contatos/Contatos.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/contatos', component: Contatos }, // meuscontatos.com/contatos
+    { path: '/', component: Home } // meuscontatos.com
+  ]
+})
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
